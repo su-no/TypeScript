0fbb8fb3103439147d29984e1b4453555dd1e088
Hyojin <0910.shj@gmail.com>
2023-05-03 00:54:44 +0900

    TypeScript Class public/private, getter/setter

--- 12.Class/2.TS_class.js
+++ 12.Class/2.TS_class.js
@@ -1,18 +1,40 @@
 var Player = /** @class */ (function () {
+    // Paramter Property 단축 구문
     function Player(first, last) {
+        this.first = first;
+        this.last = last;
         // * Public/Private property
         // 기본적으로 public이라서 생략 가능
-        this.score = 0;
+        this.age = 20;
         // private은 #와 같은 의미 (해당 메서드나 프로퍼티는 클래스 내부에서만 사용 가능)
         // private은 TypeScript이므로 컴파일 전에만 체크
         // #은 JavaScript이므로 런타임에서도 체크
-        this.age = 20;
-        this.first = first;
-        this.last = last;
+        this._score = 0;
     }
     Player.prototype.secretMethod = function () {
         console.log('SECRET METHOD!');
     };
+    Object.defineProperty(Player.prototype, "fullName", {
+        // getter만 있고 setter가 없으면 readonly
+        get: function () {
+            return "".concat(this.first, " ").concat(this.last);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "score", {
+        get: function () {
+            return this.score;
+        },
+        set: function (newScore) {
+            if (newScore < 0) {
+                throw new Error('Score cannot be less than 0');
+            }
+            this._score = newScore;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Player;
 }());
 var player1 = new Player('Marta', 'Stewart');
--- 12.Class/2.TS_class.ts
+++ 12.Class/2.TS_class.ts
@@ -1,23 +1,33 @@
 class Player {
-  // * Read-only property
-  readonly first: string;
-  readonly last: string;
-
   // * Public/Private property
   // 기본적으로 public이라서 생략 가능
-  public score: number = 0;
+  public age: number = 20;
   // private은 #와 같은 의미 (해당 메서드나 프로퍼티는 클래스 내부에서만 사용 가능)
   // private은 TypeScript이므로 컴파일 전에만 체크
   // #은 JavaScript이므로 런타임에서도 체크
-  private age: number = 20;
+  private _score: number = 0;
 
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
-  }
+  // Paramter Property 단축 구문
+  constructor(public first: string, public last: string) {}
 
   private secretMethod(): void {
     console.log('SECRET METHOD!');
+  }
+
+  // getter만 있고 setter가 없으면 readonly
+  get fullName(): string {
+    return `${this.first} ${this.last}`;
+  }
+
+  get score(): number {
+    return this.score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error('Score cannot be less than 0');
+    }
+    this._score = newScore;
   }
 }
 
